2d6c72ac5cb7a639d2e42287201792dc6a90e44f
dp <set>
2022-12-13 20:59:02 +0100

    feat: add distribution helper 2

---
+++ contracts/XDEFIDistributionHelper2.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import { IXDEFIDistributionHelper, IXDEFIDistributionLike } from "./interfaces/IXDEFIDistributionHelper.sol";
+
+/// @dev Stateful helper contract for external clients to reduce web3 calls to gather XDEFIDistribution information related to individual accounts.
+contract XDEFIDistributionHelper2 is Ownable {
+    address public xdefiAddress;
+    address public xdefiDistributionHelperAddress;
+
+    constructor() {}
+
+    function setConfig(address xdefiAddress_, address xdefiDistributionHelperAddress_) public onlyOwner {
+        xdefiAddress = xdefiAddress_;
+        xdefiDistributionHelperAddress = xdefiDistributionHelperAddress_;
+    }
+
+    function balanceOfStakedXDEFI(address account_) public view returns (uint256 totalStakedXDEFI) {
+        IXDEFIDistributionLike.Position[] memory positions;
+
+        (, positions, ) = IXDEFIDistributionHelper(xdefiDistributionHelperAddress).getAllLockedPositionsForAccount(xdefiAddress, account_);
+
+        totalStakedXDEFI = 0;
+        for (uint256 i; i < positions.length; ) {
+            totalStakedXDEFI += uint256(positions[i].depositedXDEFI);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+}
