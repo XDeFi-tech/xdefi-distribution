b231d75ef48045bffc3848d7613f8af09a3b945c
dp <set>
2023-03-21 15:55:18 +0100

    chore: simplified contract ERC20Permit included

--- contracts/XDEFIVault.sol
+++ contracts/XDEFIVault.sol
@@ -3,205 +3,32 @@
 pragma solidity =0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 /**
  * @title XDEFI Vault - for vote-escrowed XDEFI (veXDEFI)
  * @author David P. (dp@xdefi.io)
  * @notice Unaudited experimental code. Do not use in production.
  */
-contract XDEFIVault is ERC20, IERC4626 {
-    using Math for uint256;
+contract XDEFIVault is ERC4626, ERC20Permit {
+    constructor(address underlying) ERC20("vXDEFI", "vXDEFI") ERC4626(IERC20(underlying)) ERC20Permit("vXDEFI") {}
 
-    IERC20 private immutable _asset;
-    uint8 private immutable _underlyingDecimals;
-
-    constructor(IERC20 asset_) ERC20("vote-escrowed XDEFI", "vXDEFI") {
-        (bool success, uint8 assetDecimals) = _tryGetAssetDecimals(asset_);
-        _underlyingDecimals = success ? assetDecimals : 18;
-        _asset = asset_;
+    function decimals() public view virtual override(ERC20, ERC4626) returns (uint8) {
+        return ERC4626.decimals();
     }
 
     /**
-     * @dev Attempts to fetch the asset decimals. A return value of false indicates that the attempt failed in some way.
+     * @dev See {EIP-5267}.
      */
-    function _tryGetAssetDecimals(IERC20 asset_) private view returns (bool, uint8) {
-        (bool success, bytes memory encodedDecimals) = address(asset_).staticcall(abi.encodeWithSelector(IERC20Metadata.decimals.selector));
-        if (success && encodedDecimals.length >= 32) {
-            uint256 returnedDecimals = abi.decode(encodedDecimals, (uint256));
-            if (returnedDecimals <= type(uint8).max) {
-                return (true, uint8(returnedDecimals));
-            }
-        }
-        return (false, 0);
-    }
-
-    /**
-     * @dev Decimals are computed by adding the decimal offset on top of the underlying asset's decimals. This
-     * "original" value is cached during construction of the vault contract. If this read operation fails (e.g., the
-     * asset has not been created yet), a default of 18 is used to represent the underlying asset's decimals.
-     *
-     * See {IERC20Metadata-decimals}.
-     */
-    function decimals() public view virtual override(IERC20Metadata, ERC20) returns (uint8) {
-        return _underlyingDecimals + _decimalsOffset();
-    }
-
-    /** @dev See {IERC4626-asset}. */
-    function asset() public view virtual override returns (address) {
-        return address(_asset);
-    }
-
-    /** @dev See {IERC4626-totalAssets}. */
-    function totalAssets() public view virtual override returns (uint256) {
-        return _asset.balanceOf(address(this));
-    }
-
-    /** @dev See {IERC4626-convertToShares}. */
-    function convertToShares(uint256 assets) public view virtual override returns (uint256) {
-        return _convertToShares(assets, Math.Rounding.Down);
-    }
-
-    /** @dev See {IERC4626-convertToAssets}. */
-    function convertToAssets(uint256 shares) public view virtual override returns (uint256) {
-        return _convertToAssets(shares, Math.Rounding.Down);
-    }
-
-    /** @dev See {IERC4626-maxDeposit}. */
-    function maxDeposit(address) public view virtual override returns (uint256) {
-        return type(uint256).max;
-    }
-
-    /** @dev See {IERC4626-maxMint}. */
-    function maxMint(address) public view virtual override returns (uint256) {
-        return type(uint256).max;
-    }
-
-    /** @dev See {IERC4626-maxWithdraw}. */
-    function maxWithdraw(address owner) public view virtual override returns (uint256) {
-        return _convertToAssets(balanceOf(owner), Math.Rounding.Down);
-    }
-
-    /** @dev See {IERC4626-maxRedeem}. */
-    function maxRedeem(address owner) public view virtual override returns (uint256) {
-        return balanceOf(owner);
-    }
-
-    /** @dev See {IERC4626-previewDeposit}. */
-    function previewDeposit(uint256 assets) public view virtual override returns (uint256) {
-        return _convertToShares(assets, Math.Rounding.Down);
-    }
-
-    /** @dev See {IERC4626-previewMint}. */
-    function previewMint(uint256 shares) public view virtual override returns (uint256) {
-        return _convertToAssets(shares, Math.Rounding.Up);
-    }
-
-    /** @dev See {IERC4626-previewWithdraw}. */
-    function previewWithdraw(uint256 assets) public view virtual override returns (uint256) {
-        return _convertToShares(assets, Math.Rounding.Up);
-    }
-
-    /** @dev See {IERC4626-previewRedeem}. */
-    function previewRedeem(uint256 shares) public view virtual override returns (uint256) {
-        return _convertToAssets(shares, Math.Rounding.Down);
-    }
-
-    /** @dev See {IERC4626-deposit}. */
-    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
-        require(assets <= maxDeposit(receiver), "ERC4626: deposit more than max");
-
-        uint256 shares = previewDeposit(assets);
-        _deposit(_msgSender(), receiver, assets, shares);
-
-        return shares;
-    }
-
-    /** @dev See {IERC4626-mint}.
-     *
-     * As opposed to {deposit}, minting is allowed even if the vault is in a state where the price of a share is zero.
-     * In this case, the shares will be minted without requiring any assets to be deposited.
-     */
-    function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
-        require(shares <= maxMint(receiver), "ERC4626: mint more than max");
-
-        uint256 assets = previewMint(shares);
-        _deposit(_msgSender(), receiver, assets, shares);
-
-        return assets;
-    }
-
-    /** @dev See {IERC4626-withdraw}. */
-    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
-        require(assets <= maxWithdraw(owner), "ERC4626: withdraw more than max");
-
-        uint256 shares = previewWithdraw(assets);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return shares;
-    }
-
-    /** @dev See {IERC4626-redeem}. */
-    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
-        require(shares <= maxRedeem(owner), "ERC4626: redeem more than max");
-
-        uint256 assets = previewRedeem(shares);
-        _withdraw(_msgSender(), receiver, owner, assets, shares);
-
-        return assets;
-    }
-
-    /**
-     * @dev Internal conversion function (from assets to shares) with support for rounding direction.
-     */
-    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view virtual returns (uint256) {
-        return assets.mulDiv(totalSupply() + 10 ** _decimalsOffset(), totalAssets() + 1, rounding);
-    }
-
-    /**
-     * @dev Internal conversion function (from shares to assets) with support for rounding direction.
-     */
-    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view virtual returns (uint256) {
-        return shares.mulDiv(totalAssets() + 1, totalSupply() + 10 ** _decimalsOffset(), rounding);
-    }
-
-    /**
-     * @dev Deposit/mint common workflow.
-     */
-    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual {
-        // If _asset is ERC777, `transferFrom` can trigger a reentrancy BEFORE the transfer happens through the
-        // `tokensToSend` hook. On the other hand, the `tokenReceived` hook, that is triggered after the transfer,
-        // calls the vault, which is assumed not malicious.
-        //
-        // Conclusion: we need to do the transfer before we mint so that any reentrancy would happen before the
-        // assets are transferred and before the shares are minted, which is a valid state.
-        // slither-disable-next-line reentrancy-no-eth
-        SafeERC20.safeTransferFrom(_asset, caller, address(this), assets);
-        _mint(receiver, shares);
-
-        emit Deposit(caller, receiver, assets, shares);
-    }
-
-    /**
-     * @dev Withdraw/redeem common workflow.
-     */
-    function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares) internal virtual {
-        if (caller != owner) {
-            _spendAllowance(owner, caller, shares);
-        }
-
-        // If _asset is ERC777, `transfer` can trigger a reentrancy AFTER the transfer happens through the
-        // `tokensReceived` hook. On the other hand, the `tokensToSend` hook, that is triggered before the transfer,
-        // calls the vault, which is assumed not malicious.
-        //
-        // Conclusion: we need to do the transfer after the burn so that any reentrancy would happen after the
-        // shares are burned and after the assets are transferred, which is a valid state.
-        _burn(owner, shares);
-        SafeERC20.safeTransfer(_asset, receiver, assets);
-
-        emit Withdraw(caller, receiver, owner, assets, shares);
-    }
-
-    function _decimalsOffset() internal view virtual returns (uint8) {
-        return 0;
+    function eip712Domain() public view virtual returns (bytes1 fields, string memory name, string memory version, uint256 chainId, address verifyingContract, bytes32 salt, uint256[] memory extensions) {
+        return (
+            hex"0f", // 01111
+            "vXDEFI",
+            "1",
+            block.chainid,
+            address(this),
+            bytes32(0),
+            new uint256[](0)
+        );
     }
 }
---
+++ test/XDEFIVault.js
@@ -0,0 +1,120 @@
+const { expect } = require('chai');
+const { fromRpcSig } = require('ethereumjs-util');
+const ethSigUtil = require('eth-sig-util');
+
+const toWei = (value, add = 0, sub = 0) => (BigInt(value) * 1_000_000_000_000_000_000n + BigInt(add) - BigInt(sub)).toString();
+
+const EIP712Domain = [
+    { name: 'name', type: 'string' },
+    { name: 'version', type: 'string' },
+    { name: 'chainId', type: 'uint256' },
+    { name: 'verifyingContract', type: 'address' },
+    { name: 'salt', type: 'bytes32' },
+];
+
+function domainType(domain) {
+    return EIP712Domain.filter(({ name }) => domain[name] !== undefined);
+}
+
+async function getDomain(contract) {
+    const { fields, name, version, chainId, verifyingContract, salt, extensions } = await contract.eip712Domain();
+
+    if (extensions.length > 0) {
+        throw Error('Extensions not implemented');
+    }
+
+    const domain = { name, version, chainId, verifyingContract, salt };
+    for (const [i, { name }] of EIP712Domain.entries()) {
+        if (!(fields & (1 << i))) {
+            delete domain[name];
+        }
+    }
+
+    return domain;
+}
+
+const Permit = [
+    { name: 'owner', type: 'address' },
+    { name: 'spender', type: 'address' },
+    { name: 'value', type: 'uint256' },
+    { name: 'nonce', type: 'uint256' },
+    { name: 'deadline', type: 'uint256' },
+];
+
+describe('XDEFIVault', function () {
+    let XDEFI;
+    let XDEFIVault;
+    let vToken;
+    let owner;
+    let alice;
+    let account1;
+    let account2;
+    let account3;
+    let xdefiDomainSeparator;
+    const totalSupply = toWei(100000000);
+
+    const maxDeadline = Number.MAX_SAFE_INTEGER;
+    const EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA = '\x19\x01';
+    const ERC20_PERMIT_SIGNATURE_HASH = '0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9';
+    const CONSUME_PERMIT_SIGNATURE_HASH = '0xa0a7128942405265cd830695cb06df90c6bfdbbe22677cc592c3d36c3180b079';
+    const privateKey = '0x7797c0f3db8b946604ec2039dfd9763e4ffdc53174342a2ed9b14fa3eda666a5';
+    const wallet = new ethers.Wallet(privateKey, ethers.provider);
+    wallet.privateKey = privateKey;
+
+    const createErc20PermitSignature = async (owner, spender, amount, nonce, deadline) => {
+        const buildData = async (contract, deadline = maxDeadline) => {
+            const domain = await getDomain(contract);
+            return {
+                primaryType: 'Permit',
+                types: { EIP712Domain: domainType(domain), Permit },
+                domain: {
+                    ...domain,
+                    chainId: domain.chainId.toNumber(),
+                },
+                message: { owner: owner.address, spender, value: amount.toString(), nonce: nonce.toNumber(), deadline },
+            };
+        };
+
+        const data = await buildData(vToken, deadline);
+        const sigRpc = ethSigUtil.signTypedMessage(Uint8Array.from(Buffer.from(owner.privateKey.substring(2), 'hex')), { data });
+        const { v, r, s } = fromRpcSig(sigRpc);
+
+        return { v, r, s };
+    };
+
+    beforeEach(async function () {
+        [owner, alice, account1, account2, account3] = await ethers.getSigners();
+        XDEFI = await (await (await ethers.getContractFactory('XDEFI')).deploy('XDEFI', 'XDEFI', totalSupply)).deployed();
+        XDEFIVault = await ethers.getContractFactory('XDEFIVault');
+        vToken = await XDEFIVault.connect(owner).deploy(XDEFI.address);
+        XDEFIVault = await vToken.deployed();
+        vTokenAlice = await XDEFIVault.connect(alice);
+        xdefiDomainSeparator = await XDEFI.DOMAIN_SEPARATOR();
+
+        // Give each account 1000 XDEFI
+        await (await XDEFI.transfer(account1.address, toWei(1000))).wait();
+        await (await XDEFI.transfer(account2.address, toWei(1000))).wait();
+        await (await XDEFI.transfer(account3.address, toWei(1000))).wait();
+        await (await XDEFI.transfer(wallet.address, toWei(1000))).wait();
+        await (await XDEFI.transfer(alice.address, toWei(1000))).wait();
+
+        // Give 100 Ether to `accountWithPrivateKey`
+        await owner.sendTransaction({ to: wallet.address, value: ethers.utils.parseEther('100') });
+    });
+
+    it('should have correct name and symbol', async function () {
+        expect(await vToken.name()).to.equal('vXDEFI');
+        expect(await vToken.symbol()).to.equal('vXDEFI');
+    });
+
+    it('should allow vToken holders to approve a transfer', async function () {
+        const amount = 100;
+        await (await XDEFI.transfer(wallet.address, toWei(1000))).wait();
+        const deadline = Math.floor(Date.now() / 1000) + 3600;
+        const nonce = await vToken.nonces(wallet.address);
+        const { v, r, s } = await createErc20PermitSignature(wallet, vToken.address, amount, nonce, deadline);
+        // console.log({ v, r, s });
+        await (await XDEFIVault.connect(account3)).permit(wallet.address, vToken.address, amount, deadline, v, r, s);
+        expect(await vToken.allowance(wallet.address, vToken.address)).to.equal(amount);
+    });
+});
